f31a2d3393a5c6f3233477a389fc147e378d08e2
Broowl <daniel.krieger95@gmail.com>
2023-08-27 13:42:47 +0200

    add scanner

--- app/src/main/java/com/acr_mobile_scanner/SecondFragment.kt
+++ app/src/main/java/com/acr_mobile_scanner/SecondFragment.kt
@@ -5,36 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.acr_mobile_scanner.databinding.FragmentSecondBinding
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import android.content.Intent
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
+        val integrator = IntentIntegrator.forSupportFragment(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("Scan a QR code")
+        integrator.setCameraId(0)
+        integrator.setBeepEnabled(false)
+        integrator.initiateScan()
 
+        return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val result: IntentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                // Handle cancellation
+            } else {
+                // Handle scanned QR code
+            }
+        }
     }
 }
