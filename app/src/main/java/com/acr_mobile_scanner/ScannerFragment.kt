a20ce29a44eb867472f622fadf3793ef29d7b2cc
Broowl <daniel.krieger95@gmail.com>
2023-08-29 19:53:07 +0200

    Change QR Code scanner implementation

--- app/src/main/java/com/acr_mobile_scanner/ScannerFragment.kt
+++ app/src/main/java/com/acr_mobile_scanner/ScannerFragment.kt
@@ -1,46 +1,110 @@
 package com.acr_mobile_scanner
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.google.zxing.integration.android.IntentIntegrator
-import com.google.zxing.integration.android.IntentResult
-import android.content.Intent
-import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class ScannerFragment : Fragment() {
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Size
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LifecycleOwner
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import androidx.navigation.fragment.findNavController
 
+
+class ScannerFragment : Fragment() {
+    private var cameraExecutor: ExecutorService ? = null
+    private var barcodeScanner: BarcodeScanner? = null
     private val _viewModel: EntityViewModel by activityViewModels()
+    private var camera: Camera? = null
+    private var previewView: PreviewView? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_scanner, container, false)
-
-        val integrator = IntentIntegrator.forSupportFragment(this)
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
-        integrator.setPrompt("Scan a QR code")
-        integrator.setCameraId(0)
-        integrator.setBeepEnabled(false)
-        integrator.initiateScan()
-
+        previewView = view.findViewById(R.id.previewView)
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            startCamera()
+        }
         return view
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val result: IntentResult =
-            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        val scanner = _viewModel.scanner.value
-        if (result?.contents != null && scanner != null) {
-            val scanResult = scanner.processQrCode(result.contents)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        val options: BarcodeScannerOptions = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            .build()
+        barcodeScanner = BarcodeScanning.getClient(options)
+    }
+
+    private fun startCamera() {
+        val cameraProvider: ProcessCameraProvider =
+            ProcessCameraProvider.getInstance(requireContext()).get()
+        cameraProvider.unbindAll()
+        val cameraSelector: CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+        val preview = Preview.Builder()
+            .setTargetResolution(Size(640, 480))
+            .build()
+        preview.setSurfaceProvider(previewView!!.surfaceProvider)
+        val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(640, 480))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+        imageAnalysis.setAnalyzer(cameraExecutor!!) { imageProxy ->
+            scanQrCode(imageProxy)
+            imageProxy.close()
+        }
+        camera = cameraProvider.bindToLifecycle(
+            requireContext() as LifecycleOwner,
+            cameraSelector,
+            preview,
+            imageAnalysis
+        )
+    }
+
+    private fun onBarcodeScanned(barCodes:List<Barcode>){
+        for (barcode in barCodes) {
+            val scannedData: String = barcode.getRawValue()!!
+            //Toast.makeText(requireContext(), "Scanned: $scannedData", Toast.LENGTH_SHORT)
+            //   .show()
+            val scanResult =  _viewModel.scanner.value!!.processQrCode(scannedData)
             if (scanResult.isSuccess) {
                 findNavController().navigate(R.id.action_ScannerFragment_to_ResultSuccessFragment)
             }
@@ -50,4 +114,43 @@ class ScannerFragment : Fragment() {
         }
     }
 
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun scanQrCode(imageProxy: ImageProxy) {
+        val image: InputImage = InputImage.fromMediaImage(
+            imageProxy.getImage()!!,
+            imageProxy.getImageInfo().getRotationDegrees()
+        )
+        barcodeScanner!!.process(image)
+            .addOnSuccessListener { barCodes ->
+                onBarcodeScanned(barCodes)
+            }
+            .addOnFailureListener { e -> }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Camera permission denied. Cannot scan QR codes.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor!!.shutdown()
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
+    }
 }
