57ac257903228eafc444652ec62bb4b923d86177
Broowl <daniel.krieger95@gmail.com>
2023-09-03 15:19:59 +0200

    Add about and help menu

--- app/src/main/java/com/acr_mobile_scanner/MainActivity.kt
+++ app/src/main/java/com/acr_mobile_scanner/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.acr_mobile_scanner
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.fragment.app.activityViewModels
 import com.acr_mobile_scanner.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -51,22 +53,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> {
-                findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.ConfigurationFragment)
-                return true
+            R.id.action_configuration -> {
+                return onConfigurationSelected()
             }
+
+            R.id.action_about -> {
+                return onAboutSelected()
+            }
+
+            R.id.action_help -> {
+                return onHelpSelected()
+            }
+
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun onConfigurationSelected(): Boolean {
+        findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.ConfigurationFragment)
+        return true
+    }
+
+    private fun onAboutSelected(): Boolean {
+        AlertDialog.Builder(this)
+        .setMessage("Author: Daniel Krieger\n" +
+                "Version: " + resources.getString(R.string.version))
+        .setTitle("About")
+        .setCancelable(true)
+        .setPositiveButton("Ok") { _: DialogInterface, _: Int -> }
+        .create().show()
+        return true
+    }
+
+    private fun onHelpSelected(): Boolean {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Broowl/acr_mobile_scanner"))
+        startActivity(browserIntent)
+        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
